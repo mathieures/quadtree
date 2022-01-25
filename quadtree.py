b68706fdfdc72da7d9146a0917a9527e5208a9ce
mathieures <71087433+mathieures@users.noreply.github.com>
2022-01-25 10:36:05 +0100

    Renamed file

--- quadtree.py
+++ quadtree.py
@@ -2,38 +2,38 @@ from Rectangle import Rectangle, DrawnRectangle
 import color
 
 class Quadtree:
-    """Un quadtree, ou arbre quartique"""
+    """A quad tree node"""
     MAX_OBJECTS = 5
     # MAX_OBJECTS = 10
     MAX_LEVELS = 5
 
     def __init__(self, window, level, bounds):
-        self.window = window # la fenêtre où afficher les zones
-        self.level = level # 0 étant la racine
-        self.bounds = bounds # l'espace 2D que le node occupe, un Rectangle
-        self.objects = [] # liste d'objets, ici pour l'exemple des objets Rectangle
-        self.nodes = [None] * 4 # les 4 noeuds fils
+        self.window = window # the window where to draw the zones
+        self.level = level # 0 being the root
+        self.bounds = bounds # the 2D space the node is occupying (a Rectangle)
+        self.objects = [] # the objects in these bounds (Rectangle objects)
+        self.nodes = [None] * 4 # the children nodes
 
     def clear(self):
-        """Efface récursivement le Quadtree"""
+        """Clears the tree recursively"""
         self.objects.clear()
         for i, node in enumerate(self.nodes):
             if node is not None:
-                node.clear() # appelle la méthode clear() du Quadtree fils
+                node.clear() # Calls the child's clear() method
                 self.nodes[i] = None
 
     def split(self):
-        """Sépare le Quadtree en 4 noeuds Quadtree fils"""
+        """Split the node in 4 new Quadtree objects"""
         sub_width = self.bounds.width // 2
         sub_height = self.bounds.height // 2
         x, y = self.bounds.x, self.bounds.y
 
-        self.nodes[0] = Quadtree(self.window, self.level + 1, Rectangle(x + sub_width, y, sub_width, sub_height)) # en haut à droite
-        self.nodes[1] = Quadtree(self.window, self.level + 1, Rectangle(x, y, sub_width, sub_height)) # en haut à gauche
-        self.nodes[2] = Quadtree(self.window, self.level + 1, Rectangle(x, y + sub_height, sub_width, sub_height)) # en bas à gauche
-        self.nodes[3] = Quadtree(self.window, self.level + 1, Rectangle(x + sub_width, y + sub_height, sub_width, sub_height)) # en bas à droite
+        self.nodes[0] = Quadtree(self.window, self.level + 1, Rectangle(x, y, sub_width, sub_height)) # top left
+        self.nodes[1] = Quadtree(self.window, self.level + 1, Rectangle(x + sub_width, y, sub_width, sub_height)) # top right
+        self.nodes[2] = Quadtree(self.window, self.level + 1, Rectangle(x, y + sub_height, sub_width, sub_height)) # bottom left
+        self.nodes[3] = Quadtree(self.window, self.level + 1, Rectangle(x + sub_width, y + sub_height, sub_width, sub_height)) # bottom right
 
-        # On dessine les zones, pour le debug
+        # Draw the zones for debugging purposes
         DrawnRectangle(x + sub_width, y, sub_width, sub_height, color=color.RED, parent=self.window)
         DrawnRectangle(x, y, sub_width, sub_height, color=color.RED, parent=self.window)
         DrawnRectangle(x, y + sub_height, sub_width, sub_height, color=color.RED, parent=self.window)
@@ -41,31 +41,31 @@ class Quadtree:
 
     def get_index(self, rect):
         """
-        Retourne l'indice du fils dans lequel l'objet passé
-        en paramètre devra aller, ou -1 s'il n'est dans aucun
+        Returns the index of the child node the object
+        would belong to, or -1 if none correspond
         """
         index = -1
 
         vertical_midpoint = self.bounds.x + self.bounds.width // 2
         horizontal_midpoint = self.bounds.y + self.bounds.height // 2
 
-        # Est-ce que l'objet peut complètement tenir dans la moitié haute ?
+        # Does the object fit in the top half?
         in_top_half = rect.y < horizontal_midpoint and rect.y + rect.height < horizontal_midpoint
 
-        # Est-ce que l'objet peut complètement tenir dans la moitié basse ?
-        in_bottom_half = rect.y > horizontal_midpoint # on ne teste que l'origine car ce n'est que ça qui importe
+        # Does the object fit in the bottom half?
+        in_bottom_half = rect.y > horizontal_midpoint # Checking the origin is enough
 
-        # S'il peut complètement tenir dans la partie gauche
+        # If the object fits in the left half
         if rect.x < vertical_midpoint and rect.x + rect.width < vertical_midpoint:
-            # S'il tenait dans la partie haute, il est en haut à gauche
-            if in_top_half:
-                index = 1
-            elif in_bottom_half:
-                index = 2
-        # S'il peut complètement tenir dans la partie droite
-        elif rect.x > vertical_midpoint:
+            # If it fit in the top half then it's in the top left node
             if in_top_half:
                 index = 0
+            elif in_bottom_half:
+                index = 2
+        # Else if it fits in the right half
+        elif rect.x > vertical_midpoint:
+            if in_top_half:
+                index = 1
             elif in_bottom_half:
                 index = 3
 
@@ -73,26 +73,26 @@ class Quadtree:
 
     def insert(self, rect):
         """
-        Insère un objet Rectangle dans le Quadtree.
-        Si le noeud cible est rempli, il est séparé
-        et on met ses noeuds fils dans les bons noeuds
+        Inserts a Rectangle object in the quad tree.
+        If the targeted node is full, it's split and
+        its children are moved in the right nodes
         """
-        # Si le node a des enfants, on l'insère dans un enfant
+        # If the nodes has children, insert the object in one of them
         if self.nodes[0] is not None:
             index = self.get_index(rect)
             if index != -1:
                 self.nodes[index].insert(rect)
                 return
 
-        # Sinon soit l'objet ne rentre pas soit il n'y a pas d'enfant
-        # On ajoute l'objet au node
+        # Either there is no child, or the object didn't fit in a child
+        # Add the object to the node
         self.objects.append(rect)
 
         if len(self.objects) > self.MAX_OBJECTS and self.level < self.MAX_LEVELS:
             if self.nodes[0] is None:
                 self.split()
 
-            # On répartit les objets dans les enfants
+            # Distribute the objects in the children nodes if they fit
             i = 0
             while i < len(self.objects):
                 index = self.get_index(self.objects[i])
@@ -103,8 +103,8 @@ class Quadtree:
 
     def retrieve(self, rect, potential_collisions=None):
         """
-        Retourne tous les objets qui pourraient
-        collide avec l'objet donné en paramètre
+        Returns all the objects that could
+        collide with the given object
         """
         if potential_collisions is None:
             potential_collisions = []
